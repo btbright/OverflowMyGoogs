7b28287ff74cfcbabfbe95a797713e6cff279149
William Blackerby <wm@Williams-MacBook-Pro.local>
2012-11-08 19:56:30 -0600

    removed extraneous js libraries, modified manifest.json

--- scripts/bg.js
+++
@@ -1,5 +0,0 @@
-function openPopup(){
-	window.open(chrome.extension.getURL('popup.html'),"mywindow","menubar=1,resizable=1,width=350,height=250");
-}
-
-$(document).bind('keydown', 'ctrl+p', openPopup);
