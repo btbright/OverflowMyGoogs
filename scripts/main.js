f9dec0641a90b39fe276b4ac39fe8e46104739a0
Ben Bright <btbright@gmail.com>
2012-11-08 20:36:24 -0600

    updating google call

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,3 @@
 chrome.omnibox.onInputEntered.addListener(function(text) {
-  chrome.tabs.update({url:"http://google.com/#q=site:stackoverflow.com "+encodeURIComponent(text)});
+  chrome.tabs.update({url:"http://google.com/search?q=site:stackoverflow.com "+encodeURIComponent(text)});
 });
