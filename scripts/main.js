7890ea8baa2bf3e4b0c89a099b0d25e864287e11
Ben Bright <btbright@gmail.com>
2012-11-12 22:59:01 -0600

    Added "www" subdomain to google call

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,3 @@
 chrome.omnibox.onInputEntered.addListener(function(text) {
-  chrome.tabs.update({url:"http://google.com/search?q=site:stackoverflow.com "+encodeURIComponent(text)});
+  chrome.tabs.update({url:"http://www.google.com/search?q=site:stackoverflow.com "+encodeURIComponent(text)});
 });
